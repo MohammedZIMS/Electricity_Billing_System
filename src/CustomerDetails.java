008411e3310122a88b19fcc90055a9e0876235a4
Md. Zahidul Islam Mehedi <87989202+Mohammed-ZIMS@users.noreply.github.com>
2024-06-17 22:26:49 +0600

    Update CustomerDetails.java

--- src/CustomerDetails.java
+++ src/CustomerDetails.java
@@ -6,9 +6,9 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
 public class CustomerDetails extends JFrame implements ActionListener {
-    Choice searchMeterNoChoice, searchMonthChoice, searchNameChoice;
-    JLabel searchMeterNoLabel, searchMonthLabel, searchNameLabel;
-    JButton printButton, searchButton, closeButton, SHCleanButton;
+    Choice searchMeterNoChoice, searchNameChoice;
+    JLabel searchMeterNoLabel, searchNameLabel;
+    JButton printButton, searchButton, closeButton, shCleanButton;
     JTable customerTable;
     DefaultTableModel tableModel;
 
@@ -19,8 +19,8 @@ public class CustomerDetails extends JFrame implements ActionListener {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel searchPanel = new JPanel();
-        searchPanel.setBackground(new Color(144,238,144));
-        searchPanel.setLayout(new GridLayout(4, 2, 10, 10));
+        searchPanel.setBackground(new Color(144, 238, 144));
+        searchPanel.setLayout(new GridLayout(1, 5, 10, 10));
         searchPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
         add(searchPanel, BorderLayout.NORTH);
 
@@ -37,27 +37,43 @@ public class CustomerDetails extends JFrame implements ActionListener {
         searchNameChoice = new Choice();
         populateNameChoice();
         searchPanel.add(searchNameChoice);
-
-        searchMonthLabel = new JLabel("Search by Month:");
-        searchPanel.add(searchMonthLabel);
-
-        searchMonthChoice = new Choice();
-        addMonthsToChoice();
-        searchPanel.add(searchMonthChoice);
+    /*
+        JScrollPane scrollPane = new JScrollPane(customerTable);
+        scrollPane.setBounds(0,100,700,500);
+        scrollPane.setBackground(Color.white);
+        add(scrollPane);
+    */
 
         searchButton = new JButton("Search");
         searchButton.addActionListener(this);
         searchPanel.add(searchButton);
 
-        SHCleanButton = new JButton("Search History Clean");
-        SHCleanButton.addActionListener(e ->
+        shCleanButton = new JButton("Clear Search History");
+        shCleanButton.addActionListener(e -> 
         {
             tableModel.setRowCount(0);
-        }
-        );
-        searchPanel.add(SHCleanButton);
+            try {
+                DataBases c = new DataBases();
+                String query = "SELECT * FROM new_Customer";
+                ResultSet resultSet = c.statement.executeQuery(query);
+                while (resultSet.next()) {
+                    String meterNo = resultSet.getString("meterNo");
+                    String name = resultSet.getString("name");
+                    String address = resultSet.getString("address");
+                    String city = resultSet.getString("city");
+                    String state = resultSet.getString("state");
+                    String email = resultSet.getString("email");
+                    String phoneNumber = resultSet.getString("phoneNumber");
+    
+                    tableModel.addRow(new Object[]{meterNo, name, address, city, state, email, phoneNumber});
+                }
+            } catch (Exception E) {
+                E.printStackTrace();
+            }
+        });
+        searchPanel.add(shCleanButton);
 
-        tableModel = new DefaultTableModel(new String[]{"Meter No", "Name", "Address", "Unit Consumed", "Month", "Total Bill", "Status"}, 0);
+        tableModel = new DefaultTableModel(new String[]{"Meter No", "Name", "Address", "City", "State", "Email", "Phone Number"}, 0);
         customerTable = new JTable(tableModel);
         add(new JScrollPane(customerTable), BorderLayout.CENTER);
 
@@ -65,12 +81,14 @@ public class CustomerDetails extends JFrame implements ActionListener {
         printButton = new JButton("Print");
         printButton.addActionListener(this);
         printPanel.add(printButton);
-        
+
         closeButton = new JButton("Close");
         closeButton.addActionListener(this);
         printPanel.add(closeButton);
 
         add(printPanel, BorderLayout.SOUTH);
+
+        populateAllCustomers();
 
         setVisible(true);
     }
@@ -99,17 +117,32 @@ public class CustomerDetails extends JFrame implements ActionListener {
         }
     }
 
-    private void addMonthsToChoice() {
-        String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-        for (String month : months) {
-            searchMonthChoice.add(month);
+    private void populateAllCustomers() {
+        tableModel.setRowCount(0);
+        try {
+            DataBases c = new DataBases();
+            String query = "SELECT * FROM new_Customer";
+            ResultSet resultSet = c.statement.executeQuery(query);
+            while (resultSet.next()) {
+                String meterNo = resultSet.getString("meterNo");
+                String name = resultSet.getString("name");
+                String address = resultSet.getString("address");
+                String city = resultSet.getString("city");
+                String state = resultSet.getString("state");
+                String email = resultSet.getString("email");
+                String phoneNumber = resultSet.getString("phoneNumber");
+
+                tableModel.addRow(new Object[]{meterNo, name, address, city, state, email, phoneNumber});
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == searchButton) {
-            searchCustomerDetails();
+            checkMeterNoAndName();
         } else if (e.getSource() == closeButton) {
             dispose();
         } else if (e.getSource() == printButton) {
@@ -121,26 +154,41 @@ public class CustomerDetails extends JFrame implements ActionListener {
         }
     }
 
-    private void searchCustomerDetails() {
+    private void checkMeterNoAndName() {
         String meterNo = searchMeterNoChoice.getSelectedItem();
         String name = searchNameChoice.getSelectedItem();
-        String month = searchMonthChoice.getSelectedItem();
-        
-        tableModel.setRowCount(0); // Clear the table before adding new data
 
         try {
             DataBases c = new DataBases();
-            String query = "SELECT * FROM bill WHERE meter_no = '" + meterNo + "' OR name = '" + name + "' OR month = '" + month + "'";
+            String query = "SELECT * FROM new_Customer WHERE meterNo = '" + meterNo + "' AND name = '" + name + "'";
+            ResultSet resultSet = c.statement.executeQuery(query);
+            if (resultSet.next()) {
+                searchCustomerDetails(meterNo, name);
+            } else {
+                JOptionPane.showMessageDialog(this, "Please check Meter No and Name", "Warning", JOptionPane.WARNING_MESSAGE);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void searchCustomerDetails(String meterNo, String name) {
+        tableModel.setRowCount(0);
+
+        try {
+            DataBases c = new DataBases();
+            String query = "SELECT * FROM new_Customer WHERE meterNo = '" + meterNo + "' AND name = '" + name + "'";
             ResultSet resultSet = c.statement.executeQuery(query);
             while (resultSet.next()) {
-                String meterNoVal = resultSet.getString("meter_no");
+                String meterNoVal = resultSet.getString("meterNo");
                 String nameVal = resultSet.getString("name");
                 String addressVal = resultSet.getString("address");
-                String unitConsumedVal = resultSet.getString("unit_consumed");
-                String monthVal = resultSet.getString("month");
-                String totalBillVal = resultSet.getString("total_bill");
-                String statusVal = resultSet.getString("status");
-                tableModel.addRow(new Object[]{meterNoVal, nameVal, addressVal, unitConsumedVal, monthVal, totalBillVal, statusVal});
+                String cityVal = resultSet.getString("city");
+                String stateVal = resultSet.getString("state");
+                String emailVal = resultSet.getString("email");
+                String phoneNumberVal = resultSet.getString("phoneNumber");
+
+                tableModel.addRow(new Object[]{meterNoVal, nameVal, addressVal, cityVal, stateVal, emailVal, phoneNumberVal});
             }
         } catch (Exception e) {
             e.printStackTrace();
