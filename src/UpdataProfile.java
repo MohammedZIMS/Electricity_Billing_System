4903d0b358a6db1e845eb9c657013899f85b91d4
Md. Zahidul Islam Mehedi <87989202+Mohammed-ZIMS@users.noreply.github.com>
2024-07-03 20:03:07 +0600

    Update UpdataProfile.java

--- src/UpdataProfile.java
+++ src/UpdataProfile.java
@@ -1,27 +1,31 @@
 import javax.swing.*;
 import java.awt.*;
+import java.sql.ResultSet;
 
-public class UpdataProfile extends JFrame {
-    JLabel nameLabel, meterNoLabel, meterNoTextLabel, emailLabel, phoneNumberLabel, addressLabel;
-    JTextField nameTextField, emailTextField, phoneNumberTextField, addressTextField;
+public class UpdateProfile extends JFrame {
+    String meterNum;
+    String Accounttype; 
+    JLabel nameLabel, meterNoLabel, meterNoTextLabel, designationLabel, emailLabel, phoneNumberLabel, addressLabel;
+    JTextField nameTextField, designationTextField, emailTextField, phoneNumberTextField, addressTextField;
     JButton backButton, submitProfileButton;
 
-    UpdataProfile() {
-        setTitle("Profile");
+    UpdateProfile(String Accounttype, String meterNum) { 
+        this.meterNum = meterNum;
+        this.Accounttype = Accounttype; 
+
+        setTitle("Update Profile");
         setLayout(new BorderLayout());
 
         JPanel profilePanel = new JPanel(new BorderLayout());
 
-        JLabel profileHeading = new JLabel("Profile", JLabel.CENTER);
+        JLabel profileHeading = new JLabel("Update Profile", JLabel.CENTER);
         profileHeading.setFont(new Font("Times New Roman", Font.BOLD, 24));
         profilePanel.add(profileHeading, BorderLayout.NORTH);
 
         JPanel profileImagePanel = new JPanel();
-        // profileImagePanel.setBackground(Color.WHITE);
         ImageIcon profileIcon = new ImageIcon(ClassLoader.getSystemResource("ImagePariseba/Profail.png"));
         Image profileImage = profileIcon.getImage().getScaledInstance(350, 400, Image.SCALE_DEFAULT);
         JLabel profileLabel = new JLabel(new ImageIcon(profileImage));
-        profileLabel.setBounds(10, 200, 400, 450);
         profileImagePanel.add(profileLabel);
         profilePanel.add(profileImagePanel, BorderLayout.WEST);
 
@@ -31,16 +35,16 @@ public class UpdataProfile extends JFrame {
         nameLabel = new JLabel("Name:");
         nameLabel.setBounds(40, 100, 100, 20);
         profileBodyPanel.add(nameLabel);
-        
-        nameTextField = new JTextField("User Name");
+
+        nameTextField = new JTextField("");
         nameTextField.setBounds(40, 120, 150, 20);
         profileBodyPanel.add(nameTextField);
 
-        meterNoLabel = new JLabel("ID No:");
+        meterNoLabel = new JLabel("Meter No:");
         meterNoLabel.setBounds(40, 160, 100, 20);
         profileBodyPanel.add(meterNoLabel);
-        
-        meterNoTextLabel = new JLabel("");
+
+        meterNoTextLabel = new JLabel(meterNum);  
         meterNoTextLabel.setBounds(140, 160, 150, 20);
         profileBodyPanel.add(meterNoTextLabel);
 
@@ -48,34 +52,89 @@ public class UpdataProfile extends JFrame {
         emailLabel.setBounds(40, 200, 100, 20);
         profileBodyPanel.add(emailLabel);
         
-        emailTextField = new JTextField("email@example.com");
+        emailTextField = new JTextField("");
         emailTextField.setBounds(40, 220, 150, 20);
         profileBodyPanel.add(emailTextField);
-
+        
         phoneNumberLabel = new JLabel("Phone Number:");
         phoneNumberLabel.setBounds(40, 250, 100, 20);
         profileBodyPanel.add(phoneNumberLabel);
         
-        phoneNumberTextField = new JTextField("01223-456-7890");
+        phoneNumberTextField = new JTextField("");
         phoneNumberTextField.setBounds(40, 270, 150, 20);
         profileBodyPanel.add(phoneNumberTextField);
-
+        
         addressLabel = new JLabel("Address:");
         addressLabel.setBounds(40, 300, 100, 20);
         profileBodyPanel.add(addressLabel);
         
-        addressTextField = new JTextField("123 Main St");
+        addressTextField = new JTextField("");
         addressTextField.setBounds(40, 320, 150, 20);
         profileBodyPanel.add(addressTextField);
+        
+        designationLabel = new JLabel("Designation:");
+        designationLabel.setBounds(40, 350, 100, 20);
+        profileBodyPanel.add(designationLabel);
+
+        designationTextField = new JTextField("");
+        designationTextField.setBounds(40, 370, 150, 20);
+        profileBodyPanel.add(designationTextField);
 
         profilePanel.add(profileBodyPanel, BorderLayout.CENTER);
+
+        try {
+            DataBases c = new DataBases();
+            if (Accounttype.equals("Customer")) {
+                ResultSet resultSet = c.statement.executeQuery("SELECT * FROM new_Customer WHERE meterNo = '" + meterNum + "'");
+                if (resultSet.next()) {
+                    nameTextField.setText(resultSet.getString("name"));
+                    emailTextField.setText(resultSet.getString("email"));
+                    phoneNumberTextField.setText(resultSet.getString("phoneNumber"));
+                    addressTextField.setText(resultSet.getString("address"));
+                    designationLabel.setVisible(false); 
+                    designationTextField.setVisible(false); 
+                }
+            } else if (Accounttype.equals("Admin")) {
+                ResultSet resultSet = c.statement.executeQuery("SELECT * FROM newEmployer WHERE EmployerId = '" + meterNum + "'");
+                if (resultSet.next()) {
+                    nameTextField.setText(resultSet.getString("name"));
+                    emailTextField.setText(resultSet.getString("email"));
+                    phoneNumberTextField.setText(resultSet.getString("phoneNumber"));
+                    addressTextField.setText(resultSet.getString("address"));
+                    designationTextField.setText(resultSet.getString("designation")); 
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         JPanel profileButtonPanel = new JPanel();
         backButton = new JButton("Back");
         backButton.addActionListener(e -> dispose());
         profileButtonPanel.add(backButton);
 
-        submitProfileButton = new JButton("Submite");
+        submitProfileButton = new JButton("Submit");
+        submitProfileButton.addActionListener(e -> {
+            String sname = nameTextField.getText();
+            String semail = emailTextField.getText();
+            String sphone = phoneNumberTextField.getText();
+            String saddress = addressTextField.getText();
+            String sdesignation = designationTextField.getText(); 
+
+            try {
+                DataBases c = new DataBases();
+                if (Accounttype.equals("Customer")) {
+                    c.statement.executeUpdate("UPDATE new_Customer SET name = '" + sname + "', email = '" + semail + "', phoneNumber = '" + sphone + "', address = '" + saddress + "' WHERE meterNo = '" + meterNum + "'");
+                } else if (Accounttype.equals("Admin")) {
+                    String EmployerId = meterNoTextLabel.getText();
+                    c.statement.executeUpdate("UPDATE newEmployer SET name = '" + sname + "', email = '" + semail + "', phoneNumber = '" + sphone + "', address = '" + saddress + "', designation = '" + sdesignation + "' WHERE EmployerId = '" + EmployerId + "'");
+                }
+                JOptionPane.showMessageDialog(null, "User Information Updated Successfully");
+                setVisible(false);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
         profileButtonPanel.add(submitProfileButton);
 
         profilePanel.add(profileButtonPanel, BorderLayout.SOUTH);
@@ -83,12 +142,11 @@ public class UpdataProfile extends JFrame {
         add(profilePanel);
 
         setSize(700, 500);
-        setLocation(600, 200);
         setLocationRelativeTo(null);
         setVisible(true);
     }
 
     public static void main(String[] args) {
-        new UpdataProfile();
+        SwingUtilities.invokeLater(() -> new UpdateProfile("Admin", "372899")); 
     }
 }
